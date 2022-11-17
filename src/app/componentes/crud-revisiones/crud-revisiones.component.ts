dedb5b70c9b0cd5f5f1e68e786066165cedeae37
Alexander Arenas <arenasocampoalexander@gmail.com>
2022-11-16 21:23:21 -0500

    agregue revisiones

--- src/app/componentes/crud-revisiones/crud-revisiones.component.ts
+++ src/app/componentes/crud-revisiones/crud-revisiones.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { combineLatest, Observable } from 'rxjs';
+import { Mecanico } from 'src/app/interfaces/mecanico';
+import { Repuesto } from 'src/app/interfaces/repuesto.interface';
+import { Revision } from 'src/app/interfaces/revision.interface';
+import { Vehiculo } from 'src/app/interfaces/vehiculo.interface';
+import { MecanicoService } from 'src/app/servicios/mecanico/mecanico.service';
+import { RepuestoService } from 'src/app/servicios/repuesto/repuesto.service';
+import { RevisionService } from 'src/app/servicios/revision/revision.service';
+import { VehiculoService } from 'src/app/servicios/vehiculo/vehiculo.service';
+import { DialogoRevisionComponent } from '../dialogo-revision/dialogo-revision.component';
 
 @Component({
   selector: 'app-crud-revisiones',
@@ -7,15 +18,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrudRevisionesComponent implements OnInit {
 
-  constructor() { }
+  revisiones$!: Observable<Revision[]>;
+  vehiculos!: Vehiculo[];
+  repuestos!: Repuesto[];
+  mecanicos!: Mecanico[];
+  displayedColumns: string[] = ['RevisionId', 'FechaEntrada', 'FechaSalida', 'Observaciones', 'Acciones'];
+  agregarRevision: Revision = {
+    FechaEntrada: new Date(),
+    Observaciones: '',
+    RevisionId: '',
+    // FechaSalida: new Date(),
+  }
+
+  constructor(
+    private revisionServicio: RevisionService,
+    private vehiculoServicio: VehiculoService,
+    private repuestoServicio: RepuestoService,
+    private mecanicoServicio: MecanicoService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.revisionServicio.obtenerRevisiones();
+    this.vehiculoServicio.obtenerVehiculos();
+    this.mecanicoServicio.obtenerMecanicos();
+    this.repuestoServicio.obtenerRepuestos();
+    this.revisiones$ = this.revisionServicio.obtenerListaRevision();
+    combineLatest([
+      this.vehiculoServicio.obtenerListaVehiculos(),
+      this.repuestoServicio.obtenerListaRepuestos(),
+      this.mecanicoServicio.obtenerListaMecanicos(),
+    ]).subscribe({
+      next: ([vehiculos, repuestos, mecanicos]) => {
+        this.vehiculos = vehiculos;
+        this.repuestos = repuestos;
+        this.mecanicos = mecanicos;
+      }
+    })
   }
-  RevisionId="";
-  FechaEntrada="";
-  FechaSalida="";
-  Observaciones="";
-  vehiculoId="";
-  repuestoId="";
-  mecanicoId="";
+
+  abrirDialogo(revision: Revision = this.agregarRevision) {
+    const dialogoRef = this.dialog.open(DialogoRevisionComponent, {
+      width: '65vw',
+      data: {
+        revision: revision,
+        vehiculos: this.vehiculos,
+        repuestos: this.repuestos,
+        mecanicos: this.mecanicos
+      },
+    });
+    dialogoRef.afterClosed().subscribe(resultado => {
+      if (!resultado) return;
+      if (resultado.editar) {
+        this.revisionServicio.editarRevisionPorId(resultado.formulario);
+      } else this.revisionServicio.crearRevision(resultado.formulario);
+    });
+  }
+
+  eliminarRepuesto(revision: Revision) {
+    alert(`Esta eliminando revision ${revision.RevisionId}`);
+    this.revisionServicio.eliminarRevisionPorId(revision.RevisionId);
+  }
 }
--- src/app/componentes/dialogo-revision/dialogo-revision.component.ts
+++ src/app/componentes/dialogo-revision/dialogo-revision.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Mecanico } from 'src/app/interfaces/mecanico';
+import { Repuesto } from 'src/app/interfaces/repuesto.interface';
+import { Vehiculo } from 'src/app/interfaces/vehiculo.interface';
+import { ClienteComponent } from 'src/app/vistas/cliente/cliente.component';
+import { CrudRevisionesComponent } from '../crud-revisiones/crud-revisiones.component';
 
 @Component({
   selector: 'app-dialogo-revision',
@@ -7,9 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogoRevisionComponent implements OnInit {
 
-  constructor() { }
+  editar: boolean = false;
+  desdeCliente: boolean = false;
+  vehiculos!: Vehiculo[];
+  repuestos!: Repuesto[];
+  mecanicos!: Mecanico[];
+
+  formulario = new FormGroup({
+    RevisionId: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    FechaEntrada: new FormControl(new Date(), { nonNullable: true, validators: [Validators.required] }),
+    FechaSalida: new FormControl('', { nonNullable: true }),
+    Observaciones: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    vehiculoId: new FormControl('', { nonNullable: true }),
+    repuestoId: new FormControl('', { nonNullable: true }),
+    mecanicoId: new FormControl('', {nonNullable: true})
+  })
+
+  constructor(public dialogRef: MatDialogRef<CrudRevisionesComponent, ClienteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,) { }
 
   ngOnInit(): void {
+    if(this.data.revision.RevisionId) {
+      this.editar = true;
+      this.formulario.patchValue(this.data.revision);
+    }
+    this.vehiculos = this.data.vehiculos;
+    this.repuestos = this.data.repuestos;
+    this.mecanicos = this.data.mecanicos;
+    this.desdeCliente = this.data.cliente;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
